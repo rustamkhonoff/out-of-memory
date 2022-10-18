7486fcb0a9a178c7ef71a20a5da0dfa54caeb37b
rustamkhonoff <rustamkhonoff@gmail.com>
2022-10-18 16:19:55 +0500

    initial commit

---
+++ OutOfMemory/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OutOfMemory.Models;
+using System.Diagnostics;
+
+namespace OutOfMemory.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(new Test() { Name = "Works" });
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+}
+public class Test
+{
+    public string Name { get; set; }
+}
